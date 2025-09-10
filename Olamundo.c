c24db24bb5835ecef7e9eb4dd19b4f91ffeeac31
ebertgoncalves2311-sys <ebertgoncalves2311@gmail.com>
2025-09-10 13:48:21 +0000

    estudado

--- Olamundo.c
+++ Olamundo.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main(){
+    
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    printf("Entre com o número 1:  \n");
+    scanf("%d", &numero1);
+    printf("Entre com o número 2:  \n ");
+    scanf("%d", &numero2);
+
+
+
+    //operação soma 
+    soma = numero1 + numero2; 
+
+    //operação subtraçãi
+    subtracao = numero1 - numero2;
+
+    //operação multiplicação 
+    multiplicacao = numero1 * numero2; 
+
+    //operação divisão 
+    divisao = numero1 / numero2;
+
+  printf("A Soma é: %d\n",  soma);
+  printf("A Subtracao é: %d\n",  subtracao);
+  printf("A Multiplicacao é: %d\n",  multiplicacao);
+  printf("A Divisao é: %d\n",  divisao);
+
+
+
+
+
+
+
+}
