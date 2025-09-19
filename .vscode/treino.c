933ada295710013198273c6a32a84fc55f590981
ebertgoncalves2311-sys <ebertgoncalves2311@gmail.com>
2025-09-19 12:30:10 +0000

    finalizando teste if e else

---
+++ .vscode/treino.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+//Declarar variaveis produto
+char produtoA[30] = " Produto A";
+char produtoB[30] = " Produto B";
+
+unsigned int estoqueA = 1000;
+unsigned int estoqueB = 2000;
+
+float valorA = 10.50;
+float valorB = 20.40;
+
+unsigned int estoqueMinimoA = 500;
+unsigned int estoqueMinimoB = 2500;
+
+double valorTotalA;
+double valorTotalB; 
+
+int resultadoA, resultadoB;
+
+//Exibir informaçoes do produto
+printf("Produto %s tem estoque %u e o valor unitário é R$: %.2f\n", produtoA, estoqueA, valorA);
+printf("Produto %s tem estoque %u e o valor unitário é R$: %.2f\n", produtoB, estoqueB, valorB);
+
+// Comparando o valor minimo de estoque 
+
+resultadoA = estoqueA > estoqueMinimoA;
+resultadoB = estoqueB > estoqueMinimoB; 
+
+printf("O produto %s tem estoque maior mínimo %d\n", produtoA, resultadoA);
+printf("O produto %s tem estoque maior mínimo %d\n", produtoB, resultadoB);
+
+//comparando os valores totais dos produtos
+printf("O valor total de A (R$ %.2f) é maior que o valor total de B (R$%.2f):  %.2f\n", (estoqueA * valorA) > 
+                                                                                        (estoqueB * valorB), estoqueA * valorA, estoqueB * valorB);
+
+
+
+
+   
+
+    return 0;
+}
+
